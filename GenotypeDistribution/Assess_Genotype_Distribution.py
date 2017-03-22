e39f5147936062703c0eba052d94e9ee77f641f5
Unknown <sundarvenkata@ebi.ac.uk>
2017-03-22 16:33:16 +0000

    Integrated the logic to generate stratified samples

--- GenotypeDistribution/Assess_Genotype_Distribution.py
+++ GenotypeDistribution/Assess_Genotype_Distribution.py
@@ -1,38 +1,62 @@
-import pandas as pd, sys, functools, numpy as np, operator
+import pandas as pd, sys, functools, numpy as np, operator,os,gzip
 from itertools import islice, groupby, count
+from sklearn.cross_validation import train_test_split
 
 if sys.argv.__len__() != 2:
     print("Usage: Assess_Genotype_Distribution.py <VCF_FilePath>")
     sys.exit(0)
 
-vcfFileName = sys.argv[1]
+vcfDir = sys.argv[1]
 
-# Number of rows to be skipped
-numRowsToSkip = 0
-processedLinesLimit = 5000
-lineChunkSize = 100
-inputFileHandle = open(vcfFileName)
-groups = groupby(inputFileHandle, key=lambda k, line=count(): next(line) // lineChunkSize)
-foundHeaderRow = False
+targetVCFFileName = os.path.join(vcfDir, "Stratified_Sample.vcf")
+os.remove(targetVCFFileName)
+targetVCFFileHandle = open(targetVCFFileName, "a")
 
-for k, group in groups:
-    for line in group:
-        if line.strip().upper().startswith("#CHROM"):
-            foundHeaderRow = True
-            break
-        numRowsToSkip += 1
-        if numRowsToSkip >= processedLinesLimit:
-            break
-    if foundHeaderRow:
-        break
+firstPass = True
+for vcfFileName in os.listdir(vcfDir):
+    if vcfFileName.upper().endswith("VCF.GZ"):
+        vcfFileName = os.path.join(vcfDir, vcfFileName)
+        # Number of rows to be skipped
+        numRowsToSkip = 0
+        processedLinesLimit = 5000
+        lineChunkSize = 100
+        inputFileHandle = gzip.open(vcfFileName)
+        groups = groupby(inputFileHandle, key=lambda k, line=count(): next(line) // lineChunkSize)
+        foundHeaderRow = False
 
-reader = pd.read_table(vcfFileName, sep = '\t', skiprows=numRowsToSkip, chunksize=10, iterator=True, low_memory=False, engine='c')
-genotypeCountDict = {}
-vcfDF = reader.get_chunk(10)
-columnList = vcfDF.columns.tolist()
-sampleColumnList = columnList[columnList.index("FORMAT")+1:]
+        for k, group in groups:
+            for line in group:
+                if firstPass: targetVCFFileHandle.write(line)
+                if line.strip().upper().startswith("#CHROM"):
+                    foundHeaderRow = True
+                    break
+                numRowsToSkip += 1
+                if numRowsToSkip >= processedLinesLimit:
+                    break
+            if foundHeaderRow:
+                break
 
-reader = pd.read_table(vcfFileName, sep = '\t', skiprows=numRowsToSkip, chunksize=10000, iterator=True, low_memory=False, engine='c')
+        firstPass = False
+        inputFileHandle.close()
+
+        reader = pd.read_table(vcfFileName, sep = '\t', skiprows=numRowsToSkip, chunksize=10, iterator=True, low_memory=False, engine='c', compression='gzip')
+        genotypeCountDict = {}
+        vcfDF = reader.get_chunk(10)
+        columnList = vcfDF.columns.tolist()
+        sampleColumnList = columnList[columnList.index("FORMAT")+1:]
+
+        reader = pd.read_table(vcfFileName, sep = '\t', skiprows=numRowsToSkip, chunksize=10000, iterator=True, low_memory=False, engine='c', compression='gzip')
+        for chunk in reader:
+            vcfDF = chunk
+
+            # Generate stratified sample
+            featuresToLook = ["#CHROM", "INFO", "FORMAT"]
+            train, test = train_test_split(vcfDF[featuresToLook], test_size=0.001,random_state=0, train_size=0)
+            vcfDF.iloc[test.index].to_csv(targetVCFFileHandle, header=False, index=False, sep="\t")
+
+targetVCFFileHandle.close()
+
+reader = pd.read_table(targetVCFFileName, sep='\t', skiprows=numRowsToSkip, chunksize=10000, iterator=True,low_memory=False, engine='c')
 chunkNo = 0
 for chunk in reader:
     vcfDF = chunk
@@ -55,3 +79,4 @@ print("Most Dominant Genotype: {0}, Count: {1}".format(sorted_genotypeCountTuple
 print("Second most Dominant Genotype: {0}, Count: {1}".format(sorted_genotypeCountTuple[1][0], sorted_genotypeCountTuple[1][1]))
 print("Third most Dominant Genotype: {0}, Count: {1}".format(sorted_genotypeCountTuple[2][0], sorted_genotypeCountTuple[2][1]))
 print("Genotype counts: \n{0}".format(sorted_genotypeCountTuple))
+
